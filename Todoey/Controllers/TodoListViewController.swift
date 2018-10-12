9027101cc7a9d6249afe42ee906b359f94964453
Ryan Ver Maas <ryanjvm@gmail.com>
2018-10-12 13:30:06 +1000

    CRUD working in Core Data Persistence

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,114 +7,118 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
     //    when was this told to be deleted ??
     //    let defaults = UserDefaults.standard
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadItems()
-    
+        
         
         //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
         //            itemArray = items
-//         }
-    
-}
-
-// MARK - Tableview Datasource Methods
-
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return itemArray.count
-}
-
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-    let item = itemArray[indexPath.row]
-    
-    cell.textLabel?.text = item.title
-    // Ternary operator ==>
-    // value = condition ? valueIfTrue : valueIfFalse
-    
-    cell.accessoryType = item.done ? .checkmark : .none
-    // then why is the toggle here?? and not in the function below?
-    // transcribe this lesso
-
-    return cell
-}
-
-//MARK -  TableView Delegate Methods
-
-override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
-    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-    saveItems()
-    
-    // this is a point in the video that shows transcription is right where she got it wrong.
-    // this is not the place where you toggle checkmark
-    
-    tableView.deselectRow(at: indexPath, animated: true)
-}
-
-
-
-//MARK - Add New Items
-
-@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-    
-    
-    var textField = UITextField()
-    let alert = UIAlertController(title: "Add New TODOEY Item", message: "", preferredStyle: .alert)
-    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-        
-        let newItem = Item()
-        newItem.title = textField.text!
-        self.itemArray.append(newItem)
-        self.saveItems()
+        //         }
         
     }
     
-    alert.addTextField { (alertTextField) in
-        
-        alertTextField.placeholder = "Create New Item"
-        textField = alertTextField
-        //                print(alertTextField.text!)
-        
+    // MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
     
-    alert.addAction(action)
-    self.present(alert, animated: true, completion: nil)
-}
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        // Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        // then why is the toggle here?? and not in the function below?
+        // transcribe this lesso
+        
+        return cell
+    }
+    
+    //MARK -  TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // 'D' for Delete in CRUD
+        // order is important
+//        context.delete( itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        // then the saveItems() does it work
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        
+        // this is a point in the video that shows transcription is right where she got it wrong.
+        // this is not the place where you toggle checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New TODOEY Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Item(context: self.context )
+            newItem.title = textField.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            self.saveItems()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            //                print(alertTextField.text!)
+            
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
     func loadItems() {
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
         }
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         self.tableView.reloadData()
